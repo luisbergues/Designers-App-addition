9b43bba8cf0afc088e2fde621328b6571492fcf9
luisbergues <claluimor@gmail.com>
2026-07-01 13:09:18 -0300

    wish me luck
    2

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import { Toaster } from 'react-hot-toast';
+import { KpiProvider } from './context/KpiContext';
+import { Layout } from './components/Layout';
+import { DashboardView } from './views/DashboardView';
+import { ProjectsView } from './views/ProjectsView';
+import { Phase1Form } from './views/Phase1Form';
+import { Phase2Form } from './views/Phase2Form';
+
+const AppContent: React.FC = () => {
+  const [currentView, setCurrentView] = useState('dashboard');
+
+  return (
+    <Layout currentView={currentView} setCurrentView={setCurrentView}>
+      {currentView === 'dashboard' && <DashboardView />}
+      {currentView === 'projects' && <ProjectsView />}
+      {currentView === 'phase1' && <Phase1Form />}
+      {currentView === 'phase2' && <Phase2Form />}
+    </Layout>
+  );
+};
+
+function App() {
+  return (
+    <KpiProvider>
+      <AppContent />
+      <Toaster position="top-right" />
+    </KpiProvider>
+  );
+}
+
+export default App;
